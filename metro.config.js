9c5f79ad0aa1457b51df047af9ff61b15b618178
Farooq Olanrewaju <ofarooqdev@gmail.com>
2025-11-13 19:49:22 +0300

    fix(ci): build error in metro config and typescript compatibility error in i18n

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+const defaultBlockList = config.resolver.blockList || [];
+
+config.resolver.blockList = [
+  ...defaultBlockList,
+  /.*\.test\.tsx?$/,
+  /.*\.spec\.tsx?$/,
+];
+
 module.exports = config;
